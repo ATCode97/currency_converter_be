2c69b70a8d202a51089a5a7e24ca2187dfcd000b
Alistair Tsang <alistairtsang@Alistairs-MacBook-Pro.local>
2020-08-22 19:16:00 +0100

    trying to solve issue

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -3,7 +3,7 @@ const currencyRouter = require("./currencyRouter");
 const { getEndPointsInfo } = require("../controller/api");
 const { handle405s } = require("../errors");
 
-apiRouter.route("/").get(getEndPointsInfo).all(handle405s);
+apiRouter.route("/").get(getEndPointsInfo);
 
 apiRouter.use("/currencyhistory", currencyRouter);
 
