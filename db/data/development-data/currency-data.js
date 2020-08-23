f1d2e15ded43361e72f9bbc4b639254e54989ced
Alistair Tsang <alistairtsang@Alistairs-MacBook-Pro.local>
2020-08-23 16:53:13 +0100

    hope its all good

--- db/data/development-data/currency-data.js
+++ db/data/development-data/currency-data.js
@@ -1,14 +1,1 @@
-module.exports = [
-  {
-    GBP: 100,
-    foreign_currency: "USD",
-    amount: 131.41,
-    exchanged_at: 1542284514171,
-  },
-  {
-    GBP: 100,
-    foreign_currency: "AUD",
-    amount: 183.47,
-    exchanged_at: 1416140514171,
-  },
-];
+module.exports = [];
